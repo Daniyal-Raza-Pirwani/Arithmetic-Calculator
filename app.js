09275622c68d9a80a54bf401ac402561a8cef4d8
Daniyal Pirwani <daniyalpirwani120@gmail.com>
2024-09-29 12:40:06 -0700

    First Project

---
+++ app.js
@@ -0,0 +1,7 @@
+var num1 = +prompt("Enter the first number");
+var num2 = +prompt("Enter the Second number");
+document.write("<h2>Simple Arithmetic Calculator</h2>");
+document.write("Addition====>  ", num1 + num2);
+document.write("<br>","<br>","Multiplication====>  ", num1 * num2);
+document.write("<br>","<br>","Division====>  ", num1 / num2);
+document.write("<br>","<br>", "Subtraction====>  ", num1 - num2);
